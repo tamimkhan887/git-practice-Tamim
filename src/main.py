61c8a30fe1561710d06b2c5da210810d9a967543
Tamim Khan <tamimkhan887@gmail.com>
2026-06-13 16:13:17 +0600

    Multiply and Division Features Add To Main.Py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from datetime import date 
-from utils import add , subtract
+from utils import add , subtract , multiply , division
 print("Name : Md. Tamim Khan")
 print("Date: ",date.today())
 print("Addition :",add(20,10))
 print("Subtraction :",subtract(20,5))
+print("Multiply :",multiply(20,10))
+print("Division :",division(20,5))
