62bee33f20933883b2fc05e29802e365af07bc6d
Tamim Khan <tamimkhan887@gmail.com>
2026-06-13 15:36:46 +0600

    Add Basic calculator fuctions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
 from datetime import date 
+from utils import add , subtract
 print("Name : Md. Tamim Khan")
 print("Date: ",date.today())
+print("Addition :",add(20,10))
+print("Subtraction :",subtract(20,5))
